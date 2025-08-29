b6adb343d147b86f476dbb6b736242b3128f67c5
KendrickB-S <kendrik.23.bs@gmail.com>
2025-08-28 21:29:18 -0500

    primer avance trivia

---
+++ src/components/Trivia.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+function Trivia() {
+  const [preguntas, setPreguntas] = useState([]);
+  const [indice, setIndice] = useState(0);
+  const [puntaje, setPuntaje] = useState(0);
+  const [respuestaSeleccionada, setRespuestaSeleccionada] = useState(null);
+  const [terminado, setTerminado] = useState(false);
+}
+
+export default Trivia;
