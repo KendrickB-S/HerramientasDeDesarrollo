4fe4dbe9caea4fb746e1f4a2553855788d4a49fd
KendrickB-S <kendrik.23.bs@gmail.com>
2025-08-28 21:32:02 -0500

    segundo avance trivia

--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -6,6 +6,27 @@ function Trivia() {
   const [puntaje, setPuntaje] = useState(0);
   const [respuestaSeleccionada, setRespuestaSeleccionada] = useState(null);
   const [terminado, setTerminado] = useState(false);
+
+// Cargar preguntas desde la API
+  useEffect(() => {
+    fetch("APYKEY")
+      .then((res) => res.json())
+      .then((data) => {
+        const preguntasFormateadas = data.results.map((p) => {
+          const opciones = [...p.incorrect_answers];
+          const randomIndex = Math.floor(Math.random() * (opciones.length + 1));
+          opciones.splice(randomIndex, 0, p.correct_answer);
+
+          return {
+            pregunta: decodeURIComponent(p.question),
+            opciones: opciones.map((o) => decodeURIComponent(o)),
+            correcta: decodeURIComponent(p.correct_answer),
+          };
+        });
+        setPreguntas(preguntasFormateadas);
+      });
+  }, []);
+
 }
 
 export default Trivia;
