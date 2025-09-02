f266cb2fb8b390091eb804babfacec4b6125e304
KendrickB-S <kendrik.23.bs@gmail.com>
2025-09-01 20:25:34 -0500

    Primer avance Noticias

---
+++ src/components/Noticias.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+
+export default function Noticias() {
+  const [noticias, setNoticias] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+
+  
+  }
