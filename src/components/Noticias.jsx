3900672aeadfaefd941e98ba615e335741edb006
KendrickB-S <kendrik.23.bs@gmail.com>
2025-09-02 21:42:06 -0500

    Avance noticias API

--- src/components/Noticias.jsx
+++ src/components/Noticias.jsx
@@ -5,6 +5,9 @@ export default function Noticias() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  // ⚠️ Necesitas tu API_KEY de https://newsapi.org
+  const API_KEY = "TU_API_KEY";
+  const URL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
 
   
   }
