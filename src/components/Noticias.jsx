dde8c5e750707b3e3ddfd6e7ee85b66ecd50ddcb
KendrickB-S <kendrik.23.bs@gmail.com>
2025-09-03 20:41:20 -0500

    modificacion de seguridad

--- src/components/Noticias.jsx
+++ src/components/Noticias.jsx
@@ -6,7 +6,7 @@ export default function Noticias() {
   const [error, setError] = useState(null);
 
   // ⚠️ Necesitas tu API_KEY de https://newsapi.org
-  const API_KEY = "TU_API_KEY";
+  const API_KEY = "TU_API_KEY_AQUI";
   const URL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
 
   useEffect(() => {
