ef06e7ded0a9b88164bbd0644e2a319e11736974
KendrickB-S <kendrik.23.bs@gmail.com>
2025-09-04 21:59:06 -0500

    avance graficos estadisticas

---
+++ src/components/Estadisticas.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  PieChart,
+  Pie,
+  Cell,
+  ResponsiveContainer
+} from "recharts";
