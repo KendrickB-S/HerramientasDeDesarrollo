deeffe490672130080d3a207979ff8de4cb3ea0b
KendrickB-S <kendrik.23.bs@gmail.com>
2025-09-09 21:11:55 -0500

    avance calendario

--- src/components/Calendario.jsx
+++ src/components/Calendario.jsx
@@ -7,3 +7,14 @@ import getDay from "date-fns/getDay";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { es } from "date-fns/locale";
 
+const locales = {
+  es: es,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek: () => startOfWeek(new Date(), { weekStartsOn: 1 }),
+  getDay,
+  locales,
+});
