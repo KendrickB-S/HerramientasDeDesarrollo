3e0e18b986bdd7167ab6d44b812522082ac00774
KendrickB-S <kendrik.23.bs@gmail.com>
2025-09-30 21:03:21 -0500

    segundo avance gastos

--- src/components/Gastos.jsx
+++ src/components/Gastos.jsx
@@ -8,5 +8,21 @@ function Gastos() {
   const [monto, setMonto] = useState("");
   const [restante, setRestante] = useState(0);
 
+   // ---- Cargar datos desde localStorage
+  useEffect(() => {
+    const presupuestoGuardado = JSON.parse(localStorage.getItem("presupuesto")) || 0;
+    const gastosGuardados = JSON.parse(localStorage.getItem("gastos")) || [];
+
+    setPresupuesto(presupuestoGuardado);
+    setGastos(gastosGuardados);
+  }, []);
+
+  // ---- Guardar en localStorage cuando cambien
+  useEffect(() => {
+    localStorage.setItem("presupuesto", JSON.stringify(presupuesto));
+    localStorage.setItem("gastos", JSON.stringify(gastos));
+    calcularRestante();
+  }, [presupuesto, gastos]);
+
 
 }
