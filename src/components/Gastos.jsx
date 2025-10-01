3236b3ab9e31e824f912f58ed1e44a67edc9913b
KendrickB-S <kendrik.23.bs@gmail.com>
2025-09-30 21:02:19 -0500

    primer avance gastos

---
+++ src/components/Gastos.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Gastos() {
+  const [presupuesto, setPresupuesto] = useState(0);
+  const [gastos, setGastos] = useState([]);
+  const [descripcion, setDescripcion] = useState("");
+  const [monto, setMonto] = useState("");
+  const [restante, setRestante] = useState(0);
+
+
+}
